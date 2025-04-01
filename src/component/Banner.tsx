4509c1040bb0b29ce4b3360ced30fefaef0b07b3
Dinh Minh <minhluuu2504@gmail.com>
2025-04-01 14:33:00 +0700

    Import Tailwind

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -1,21 +1,19 @@
-import "./Banner.css";
-
 const Banner = () => {
   return (
-      <div className="banner-wrapper">
-        <div className="banner-container">
-          <div className="banner-text">
-            Freeship đơn từ 45k, giảm nhiều hơn cùng
-          </div>
-          <picture className="webpimg-container">
-            <img
-              src="https://salt.tikicdn.com/ts/upload/a7/18/8c/910f3a83b017b7ced73e80c7ed4154b0.png"
-              alt="icon"
-              className="title-img"
-            />
-          </picture>
+    <div className="relative z-[999] w-full">
+      <div className="bg-green-100 p-2 flex items-center justify-center gap-2">
+        <div className="text-green-600 text-center">
+          Freeship đơn từ 45k, giảm nhiều hơn cùng
         </div>
+        <picture className="flex items-center">
+          <img
+            src="https://salt.tikicdn.com/ts/upload/a7/18/8c/910f3a83b017b7ced73e80c7ed4154b0.png"
+            alt="icon"
+            className="max-w-[80px] h-auto"
+          />
+        </picture>
       </div>
+    </div>
   );
 };
 
