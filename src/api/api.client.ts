cd4842bfbcc16a3fa27c2ef527dda6fb7bf29f56
Dinh Minh <minhluuu2504@gmail.com>
2025-04-22 16:06:59 +0700

    Deployed

--- src/api/api.client.ts
+++ src/api/api.client.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
     },
 });
+
 apiClient.interceptors.request.use(function (config) {
     const token = localStorage.getItem("accessToken");
     config.headers.Authorization = token ? `Bearer ${token}` : "";
@@ -14,6 +15,7 @@ apiClient.interceptors.request.use(function (config) {
 }, function (error) {
     return Promise.reject(error);
 });
+
 apiClient.interceptors.response.use(function (response) {
     if (response.data) {
         return response.data;
@@ -21,6 +23,6 @@ apiClient.interceptors.response.use(function (response) {
     return response;
 }, function (error) {
     return Promise.reject(error);
-}
-);
+});
+
 export default apiClient;
